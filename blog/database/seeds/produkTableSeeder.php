e649ca53eb5cb6e445c6256072ac6d82c7356f81
suryanemo <suryajunior99@gmail.com>
2019-03-15 11:05:18 +0700

    publish file blog

---
+++ blog/database/seeds/produkTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class produkTableSeeder extends Seeder
+{
+    
+    public function run()
+    {
+        DB::table('barangs')->insert(array(['nama' => 'Meja', 'id_kategori' => '1', 'qty' =>
+'12', 'harga_beli' => '50000', 'harga_jual' => '540000', ],['nama' => 'Kursi', 'id_kategori'
+=> '1', 'qty' => '12', 'harga_beli' => '40000', 'harga_jual' => '450000', ] ));
+    }
+}
