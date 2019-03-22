1381c7636f36185fc098d9b228f4b8b1bbce20da
suryanemo <suryajunior99@gmail.com>
2019-03-22 09:43:08 +0700

    publish file blog

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,11 +11,59 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/welcome', function () {
+echo "Welcome";
 });
+
 
 Route::get('/hello', function () {
 	return '<center><h1>hello world</h1>
 	</center>';
 });
+
+route::get('/index', function () {
+	echo "uji coba route dengan methode GET";
+});
+
+//route::post ('/store', function () {
+	//sintak untuk menyimpan data
+
+//});
+
+
+//route::put('/update', function (){
+	//sintak untuk update data
+///});
+
+//route::delete ('/delete', function () { 
+ //sintak untuk menghapus data
+ //});
+
+ //Route::match(['get','post'],'/welcome' function () {
+// untuk merespon beberapa http
+//});
+
+ //Route::any('/welcome' function () {
+// untuk merespon semua http
+//});
+
+
+
+route::get('/show/{id?}', function ($id=1) { 
+	echo "Nilai Parameter Adalah ".$id;
+});
+
+route::get('/edit/{nama}', function ($nama) {
+	echo "nama Parameter  adalah ".$nama;
+})->where('nama', '[A-Za-z]+');
+
+route::get('/index', function() {
+	echo "<a href=".route('create')."'>akses route dengan nama </a>";
+});
+
+route::get('/create', function () {
+	echo "route diakses menggunakan nama";
+
+	})->name('create');
+
+route::get('/produk', 'produkController@index');
